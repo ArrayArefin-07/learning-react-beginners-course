d1189d25d0aff4120fe0cbbeafb51ce021337ab9
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-25 15:49:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
+
+function App() {
+  return(
+    // <Header></Header>
+    // <Header/> //ShortHand Way
+    <>
+    <Header/>   
+    <Food/>
+    <Footer/>
+    </>
+    
+  );
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+  return(
+    <footer>
+      <p>&copy; {new Date().getFullYear()} your Website name</p>
+    </footer>
+  )
+}
+
+export default Footer
