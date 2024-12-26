9f88a37da3c295ecc267ed5de37ec9dc60d7bc2f
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-26 20:45:37 +0600

    learning event parameter
    learning click event using if else condition in react

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,13 +2,25 @@
 
 function Button(){
 
+  let count = 0;
+
   // const handleClick = () => console.log("OUCH!");
-  const handleClick2 = (name) => console.log(`${name} stop clicking me broo..!`);
+  // const handleClick2 = (name) => console.log(`${name} stop clicking me broo..!`);
+  const handleClick = (name) => {
+    if(count < 3){
+      count++;
+      console.log(`${name} you clicked me ${count} time/s`)
+    }
+    else{
+      console.log(`${name} stop clicking me broo..!`);
+    }
+  };
 
   return(
     
-    <button onClick={() => handleClick2("Arefin")}>Click me 😃</button> 
+    // <button onClick={() => handleClick2("Arefin")}>Click me 😃</button> 
     //  <button onClick={handleClick2("Arefin")}>Click Me 😄</button> 
+    <button onClick={() => handleClick("Arefin")}>Click me 😃</button>
     
    
   )
