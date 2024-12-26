8c760ff228bdceb46d1db55f8c5e5ce6025773c1
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-26 20:58:49 +0600

    learning event parameter & double click event

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,25 +2,32 @@
 
 function Button(){
 
-  let count = 0;
+  // let count = 0;
 
   // const handleClick = () => console.log("OUCH!");
   // const handleClick2 = (name) => console.log(`${name} stop clicking me broo..!`);
-  const handleClick = (name) => {
-    if(count < 3){
-      count++;
-      console.log(`${name} you clicked me ${count} time/s`)
-    }
-    else{
-      console.log(`${name} stop clicking me broo..!`);
-    }
-  };
+  // const handleClick = (name) => {
+  //   if(count < 3){
+  //     count++;
+  //     console.log(`${name} you clicked me ${count} time/s`)
+  //   }
+  //   else{
+  //     console.log(`${name} stop clicking me broo..!`);
+  //   }
+
+  // const handleClick = (e) => console.log(e);
+  const handleClick = (e) => e.target.textContent = "OUCH 🙄";
+
+
+ 
 
   return(
     
     // <button onClick={() => handleClick2("Arefin")}>Click me 😃</button> 
     //  <button onClick={handleClick2("Arefin")}>Click Me 😄</button> 
-    <button onClick={() => handleClick("Arefin")}>Click me 😃</button>
+    // <button onClick={() => handleClick("Arefin")}>Click me 😃</button>
+    // <button onClick={(e) => handleClick(e)}>Click me 😃</button>
+    <button onDoubleClick={(e) => handleClick(e)}>Click me 😃</button>
     
    
   )
