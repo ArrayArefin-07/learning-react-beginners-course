738b55c3e2bf8475aab3ae317d7f4fda157d68ee
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-26 19:43:53 +0600

    start learning click event in react

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@
 // import Button from "./Button/Button";
 // import Student from "./Student";
 // import UserGreeting from "./UserGreeting";
-import List from "./List";
+// import List from "./List";
+import Button from "./Button";
 
 // function App() {
 //   return(
@@ -65,32 +66,40 @@ import List from "./List";
 
 //render lists
 
+// function App() {
+//   const fruits = [
+//     { id: 1, name: "apple", calories: 95 },
+//     { id: 2, name: "orange", calories: 45 },
+//     { id: 3, name: "banana", calories: 105 },
+//     { id: 4, name: "coconut", calories: 159 },
+//     { id: 5, name: "pineapple", calories: 37 },
+//   ];
+
+//   const vegetables = [
+//     { id: 6, name: "potatoes", calories: 110 },
+//     { id: 7, name: "celery", calories: 15 },
+//     { id: 8, name: "carrots", calories: 25 },
+//     { id: 9, name: "corn", calories: 63 },
+//     { id: 10, name: "broccoli", calories: 50 },
+//   ];
+
+//   return(
+//     <>
+//     <List items={fruits} category="Fruits" />;
+//     <List items={vegetables} category="Vegetables"/>;
+
+//     {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+//     {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
+//     </>
+//   ) 
+// }
+
+
+//cclick event  
 function App() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 105 },
-    { id: 4, name: "coconut", calories: 159 },
-    { id: 5, name: "pineapple", calories: 37 },
-  ];
-
-  const vegetables = [
-    { id: 6, name: "potatoes", calories: 110 },
-    { id: 7, name: "celery", calories: 15 },
-    { id: 8, name: "carrots", calories: 25 },
-    { id: 9, name: "corn", calories: 63 },
-    { id: 10, name: "broccoli", calories: 50 },
-  ];
-
-  return(
-    <>
-    <List items={fruits} category="Fruits" />;
-    <List items={vegetables} category="Vegetables"/>;
-
-    {fruits.length > 0 && <List items={fruits} category="Fruits" />}
-    {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
-    </>
-  ) 
+  return(<>
+  <Button/>
+  </>);
 }
 
 export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+
+
+function Button(){
+
+  const handleClick = () => console.log("OUCH!");
+
+  return(
+    <button onClick={handleClick}>Click me 😃</button>
+  )
+}
+
+export default Button
