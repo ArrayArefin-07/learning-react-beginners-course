e895b3532c097c7d199e3d6af446b5ea2e4f303a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-31 00:17:28 +0600

    learning update array of object in state using user input

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -93,11 +93,20 @@ function Counter(){
     const [carModel, setCarModel] = useState("");
 
     function handleAddCar(){
+      const newCar = {
+        year: carYear,
+        make: carMake,
+        model: carModel
+      }
+      setCars(c => [...c, newCar]);
 
+      setCarYear(new Date().getFullYear());
+      setCarMake("");
+      setCarModel("");
     }
 
     function handleRemoveCar(index){
-      
+     setCar(c => c.filter((_, i) => i !== index));
     }
 
     function handleYearChange(event){
@@ -105,11 +114,11 @@ function Counter(){
     }
 
     function handleMakeChange(event){
-      
+      setCarMake(event.target.value);
     }
 
     function handleMdelChange(event){
-      
+      setCarModel(event.target.value);
     }
 
   return(
@@ -175,12 +184,15 @@ function Counter(){
       <div>
           <h2>List of Car Objects</h2>
           <ul>
-
+            {cars.map((car, index) => 
+            <li key={index} onClick={() => handleRemoveCar(index)}>
+              {car.year} {car.make} {car.model}
+            </li>)}
           </ul>
 
           <input type="number" value={carYear} onChange={handleYearChange} /> <br/>
-          <input type="number" value={carMake} onChange={handleMakeChange} placeholder="Enter car make" /> <br/>
-          <input type="number" value={carModel} onChange={handleModelChange} placeholder="Enter car model" /> <br/>
+          <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Enter car make" /> <br/>
+          <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model" /> <br/>
           
           <button onClick={handleAddCar}>Add Car</button>
       </div>
